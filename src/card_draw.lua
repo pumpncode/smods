a5b50e27f1bc0c85d9c11913bc108f13a8acf582
Nano Miratus <nanomiratus@gmail.com>
2025-05-08 03:47:56 +0200

    fix crash when `floating_sprite` is `nil`

--- src/card_draw.lua
+++ src/card_draw.lua
@@ -332,13 +332,15 @@ SMODS.DrawStep {
 
             if type(self.config.center.soul_pos.draw) == 'function' then
                 self.config.center.soul_pos.draw(self, scale_mod, rotate_mod)
-            elseif self.ability.name == 'Hologram' then
-                self.hover_tilt = self.hover_tilt*1.5
-                self.children.floating_sprite:draw_shader('hologram', nil, self.ARGS.send_to_shader, nil, self.children.center, 2*scale_mod, 2*rotate_mod)
-                self.hover_tilt = self.hover_tilt/1.5
-            else
-                self.children.floating_sprite:draw_shader('dissolve',0, nil, nil, self.children.center,scale_mod, rotate_mod,nil, 0.1 + 0.03*math.sin(1.8*G.TIMERS.REAL),nil, 0.6)
-                self.children.floating_sprite:draw_shader('dissolve', nil, nil, nil, self.children.center, scale_mod, rotate_mod)
+            elseif self.children.floating_sprite then
+                if self.ability.name == 'Hologram' then
+                    self.hover_tilt = self.hover_tilt*1.5
+                    self.children.floating_sprite:draw_shader('hologram', nil, self.ARGS.send_to_shader, nil, self.children.center, 2*scale_mod, 2*rotate_mod)
+                    self.hover_tilt = self.hover_tilt/1.5
+                else
+                    self.children.floating_sprite:draw_shader('dissolve',0, nil, nil, self.children.center,scale_mod, rotate_mod,nil, 0.1 + 0.03*math.sin(1.8*G.TIMERS.REAL),nil, 0.6)
+                    self.children.floating_sprite:draw_shader('dissolve', nil, nil, nil, self.children.center, scale_mod, rotate_mod)
+                end
             end
             if self.edition then 
                 for k, v in pairs(G.P_CENTER_POOLS.Edition) do
